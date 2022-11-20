3a29e15015e08ffc5a8de969b4397da486d71542
arturogutierrez11 <arturogutierrez@Arturos-MacBook-Pro.local>
2022-11-20 18:24:25 -0300

    F

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import {store} from './redux/store/index'
 import axios from 'axios';
-import dotenv from "dotenv";
+
 
 
 axios.defaults.baseURL = process.env.CLIENT_API || "http://localhost:3001";
