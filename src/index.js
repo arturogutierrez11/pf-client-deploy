5b4d15d9b948ac314b6efd8663fe934dfde5e6c6
arturogutierrez11 <arturogutierrez@Arturos-MacBook-Pro.local>
2022-11-20 18:17:02 -0300

    first commit 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import {store} from './redux/store/index'
 import axios from 'axios';
 import dotenv from "dotenv";
-dotenv.config();
+
 
 axios.defaults.baseURL = process.env.CLIENT_API || "http://localhost:3001";
 
